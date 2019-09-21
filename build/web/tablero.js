0f9926f84d79fa1323ee0f4ab5e261fac4926e3c
Mattius Cardona <Sherlok128@gmail.com>
2019-09-21 00:22:28 -0500

    Proyecto casi terminado, solo falta modificar los estilos, las funcionalidades están completas

--- build/web/tablero.js
+++ build/web/tablero.js
@@ -1,10 +1,37 @@
-var canvas = document.getElementById("myCanvas");
+var canvas = document.getElementById("myCanvas");//Milienzo
 var clear = document.getElementById("clearBtn");
 var download = document.getElementById("downloadBtn");
-var context = canvas.getContext("2d");
-canvas.addEventListener("click", defineImage, false);
+var context = canvas.getContext("2d");//contexto
+var flagPaint=false; //Para saber si pintar o no
+var position; // la posición actual donde el usuario hizo click
+
+canvas.addEventListener('mousedown', changeFlag, false);
+canvas.addEventListener('mouseup', changeFlag, false);
+canvas.addEventListener('mousemove', defineImage, false);
 clear.addEventListener("click", clearTable, false);
 download.addEventListener("click", downloadImage, false);
+
+//Funcion para saber si se debe pintar o no
+function changeFlag(evt){
+    flagPaint=!flagPaint;
+    position=getCoords(evt);
+}
+function getCoords(evt){
+    var posX;
+    var posY;
+
+    if (evt.pageX || evt.pageY) {
+            posX = evt.pageX- canvas.getBoundingClientRect().left;
+            posY = evt.pageY- canvas.getBoundingClientRect().top;
+    }else{
+            posX = evt.clientX - canvas.getBoundingClientRect().left;
+            posY = evt.clientY - canvas.getBoundingClientRect().top;
+    }
+
+    return {x:posX,
+            y:posY
+    };
+}
 //Funcion para descargar la imagen
 function downloadImage(){
     //convierte el canva en una imagen
@@ -23,56 +50,62 @@ function getCurrentPos(evt) {
 }
 //Funcion para tomar el color y la forma desde el formulario HTML5
 function defineImage(evt) {
-    var currentPos = getCurrentPos(evt);
-
-    for (i = 0; i < document.inputForm.color.length; i++) {
-        if (document.inputForm.color[i].checked) {
-            var color = document.inputForm.color[i];
-            break;
+    if(flagPaint){
+        for (i = 0; i < document.inputForm.color.length; i++) {
+            if (document.inputForm.color[i].checked) {
+                var color = document.inputForm.color[i];
+                break;
+            }
         }
-    }
-    for (i = 0; i < document.inputForm.shape.length; i++) {
-        if (document.inputForm.shape[i].checked) {
-            var shape = document.inputForm.shape[i];
-            break;
-        }
-    }
-    //Armamos la estructura del JSON a usar
-    var json = JSON.stringify({
-        "shape": shape.value,
-        "color": color.value,
-        "coords": {
-            "x": currentPos.x,
-            "y": currentPos.y
-        }
+        var coords=getCoords(evt);
+        //Se define la estructura del Json
+        var json = JSON.stringify({
+            "shape": "square",
+            "color": color.value,
+            "coords": {
+                "x": position.x,
+                "y": position.y
+            },
+            "coordsNext": {
+                "x": coords.x,
+                "y": coords.y
+            },
+            "clr": "false"
         });
-    drawImageText(json);
-    sendText(json);
+        position={
+            x:coords.x,
+            y:coords.y
+        };
+        drawImageText(json);
+        sendText(json);
+    }
 }
 function clearTable(evt){
-    document.inputForm.color[4].checked = true;
-    document.inputForm.shape[0].checked = true;
-    defineImage(evt);
-    document.inputForm.color[0].checked = true;
+    var json = JSON.stringify({
+        "shape": "square",
+        "color": "#000000",
+        "coords": {
+            "x": 0,
+            "y": 0
+        },
+        "coordsNext": {
+            "x": 0,
+            "y": 0
+        },
+        "clr": "true"
+    });
+    drawImageText(json);
+    sendText(json);
 }
 function drawImageText(image) {
     var json = JSON.parse(image);
     context.fillStyle = json.color;
-    switch (json.shape) {
-        case "circle":
-        context.beginPath();
-        context.arc(json.coords.x, json.coords.y, 5, 0, 2 * Math.PI, false);
-        context.fill();
-        break;
-        case "square":
-        default:
-            if(json.color==="#FFFFFF"){
-                //Se define un punto del tamaño del canvas para "borrar" todo
-                context.fillRect(0, 0, canvas.width, canvas.height);
-            }
-            else{
-                context.fillRect(json.coords.x, json.coords.y, 10, 10);
-            }
-        break;
+    context.beginPath(); // comenzamos a dibujar
+    context.moveTo(json.coords.x, json.coords.y); // ubicamos el cursor en la posicion (10,10)
+    context.lineTo(json.coordsNext.x,json.coordsNext.y);
+    context.strokeStyle = json.color; // color de la linea
+    context.stroke(); // dibujamos la linea
+    if(json.clr=="true"){
+        context.clearRect(0, 0, canvas.width, canvas.height);
     }
 }
